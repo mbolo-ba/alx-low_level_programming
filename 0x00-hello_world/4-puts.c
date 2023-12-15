89925c6c1a46bee71a0b968da9e027d47497ac2d
David <davesus33@gmail.com>
2023-12-14 17:43:39 -0800

    printf !

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a string of letters
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i[] = "Programming is like building a multilingual puzzle";
+
+	puts(i);
+	return (0);
+}
