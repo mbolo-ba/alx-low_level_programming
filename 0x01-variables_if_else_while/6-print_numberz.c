2db483f35e1fd5fd10bef5237b2e146304dfef7b
David <davesus33@gmail.com>
2023-12-15 19:01:00 -0800

    I-IX

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: 0(success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
