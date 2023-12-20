1eba2c2cb24c12c04ab9fc044a6ce115ae930c8e
David <davesus33@gmail.com>
2023-12-20 10:27:27 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+i#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ * Return: 0(success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+}
