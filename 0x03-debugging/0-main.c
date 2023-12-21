bd962283f318b90a804638f08aa0e859bb65ec09
David <davesus33@gmail.com>
2023-12-21 15:30:42 -0800

    pos_or_neg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include "main.h"
 /**
  * main - tests an input with positive_or_negative()
  * Return: 0(success)
@@ -11,6 +11,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	positive_or_negative(0);
+	positive_or_negative(n);
 	return (0);
 }
